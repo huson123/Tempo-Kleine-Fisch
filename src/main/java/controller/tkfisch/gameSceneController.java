0940557aa538976d71452f66ee962155f0a6759f
TanHien <hientanphuctran@gmail.com>
2025-01-22 08:37:01 +0100

    add background to game scene

--- src/main/java/controller/tkfisch/gameSceneController.java
+++ src/main/java/controller/tkfisch/gameSceneController.java
@@ -1,6 +1,7 @@
 package controller.tkfisch;
 
-import javafx.event.ActionEvent;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -9,7 +10,9 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -20,6 +23,35 @@ public class gameSceneController {
 
     @FXML
     ImageView diceImage;
+
+    @FXML
+    AnchorPane pane;
+
+    Timeline timeline = new Timeline();
+    String gameBackgroundURL = "/animation/gameBackground/";
+
+    public void initialize() throws IOException {
+        addBackgroundToTimeline(gameBackgroundURL,12,0.2);
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
+    public void addBackgroundToTimeline(String imgURL, int frames, double duration) throws IOException {
+        for (int i =0; i<= frames; i++){
+            Image img;
+            if (i < 10){
+                img = new Image(getClass().getResourceAsStream(imgURL + "frame000" + i + ".png"));
+            }
+            else {
+                img = new Image(getClass().getResourceAsStream(imgURL + "frame00" + i + ".png"));
+            }
+            Background bg = new Background(new BackgroundImage(img, BackgroundRepeat.ROUND, BackgroundRepeat.ROUND, BackgroundPosition.CENTER, BackgroundSize.DEFAULT));
+            KeyFrame keyFrame = new KeyFrame(Duration.seconds(i * duration), // Display each image for x seconds
+                    e -> pane.setBackground(bg));
+            timeline.getKeyFrames().add(keyFrame);
+        }
+    };
+
 
 
     public void switchToDice(MouseEvent event) throws IOException {
