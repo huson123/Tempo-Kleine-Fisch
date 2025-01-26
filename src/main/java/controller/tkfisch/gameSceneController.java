094a3a143727d8af2fc45639e8f6d1c0ea8d1d77
Tanhien1 <hientptran12@gmail.com>
2025-01-26 15:41:35 +0100

    add working dice in game scene

--- src/main/java/controller/tkfisch/gameSceneController.java
+++ src/main/java/controller/tkfisch/gameSceneController.java
@@ -4,9 +4,14 @@ import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class gameSceneController implements SceneController {
-    private Controller appController;
+    private Stage stage = new Stage();
+    private Controller appController = new Controller(stage);
+    private String diceIdleUrl = "/animation/dice/diceIdle/";
     @FXML
     private ImageView ship;
     @FXML
@@ -17,9 +22,11 @@ public class gameSceneController implements SceneController {
     private ImageView pinkFish;
     @FXML
     private ImageView yellowFish;
+    @FXML
+    private ImageView dice;
 
     diceSceneController diceSC = new diceSceneController();
-    private String resultColor = diceSC.getResultColor(); ;
+    private String resultColor = diceSC.getResultColor();
 
     @Override
     public void setAppController(Controller appController) {
@@ -30,7 +37,16 @@ public class gameSceneController implements SceneController {
         //TODO add background animation
     }
 
-    public void dicePressed(MouseEvent event) {
+    public void dicePressed(MouseEvent event) throws IOException {
+
         appController.switchToScene("dice");
+    }
+    public void diceEntered(MouseEvent event) throws IOException {
+        System.out.println("dice hovered");
+        appController.playAnimation(diceIdleUrl,12, 0.2, dice);
+    }
+    public void diceExited(MouseEvent event){
+        System.out.println("dice exited");
+        dice.setImage(new Image(getClass().getResourceAsStream("/image/dice.png")));
     }
 }
