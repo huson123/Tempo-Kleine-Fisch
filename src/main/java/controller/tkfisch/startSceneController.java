1f38557edf7f8f7e801191835e7784d5fcfc6a53
huson123 <152352017+huson123@users.noreply.github.com>
2025-02-02 01:48:57 +0100

    Update startSceneController.java

--- src/main/java/controller/tkfisch/startSceneController.java
+++ src/main/java/controller/tkfisch/startSceneController.java
@@ -6,8 +6,11 @@ import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 public class startSceneController implements SceneController {
+   private selectSceneController sceneSC;
    private Controller appController;
    @FXML
    AnchorPane pane;
@@ -25,13 +28,26 @@ public class startSceneController implements SceneController {
       tl.play();
       pane.setOpacity(1);
    }
-   public void switchToSelect(ActionEvent event) {
+   public void switchToSelect(ActionEvent event) throws IOException {
+      music();
+      sceneSC = (selectSceneController) appController.getSceneController("select");
+      sceneSC.init();
       appController.switchToScene("select");
    }
    public void switchToInstruction(ActionEvent event) {
+      music();
       appController.switchToScene("instruction");
    }
    public void Exit(ActionEvent event) {
       System.exit(0);
+   }
+
+   MediaPlayer mediaPlayer;
+   public void music(){
+      String s = getClass().getResource("/music/tap.mp3").toExternalForm();
+      Media h = new Media(s);
+      mediaPlayer = new MediaPlayer(h);
+      mediaPlayer.setVolume(0.1);
+      mediaPlayer.play();
    }
 }
