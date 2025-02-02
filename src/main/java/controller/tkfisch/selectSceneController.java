eda1c4fe9496b0a0d691c997671b1ba3e97d4acf
Tanhien1 <hientptran12@gmail.com>
2025-02-02 18:03:46 +0100

    change fish and ship image in select scene and change their choosing visual

--- src/main/java/controller/tkfisch/selectSceneController.java
+++ src/main/java/controller/tkfisch/selectSceneController.java
@@ -3,22 +3,40 @@ package controller.tkfisch;
 import java.io.IOException;
 
 import backend.Gameplay;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 public class selectSceneController implements SceneController {
     private Controller appController;
     private gameSceneController gameSC;
+    private String bg = "/animation/background/start/";
 
     @FXML
-    public void init(){};
+    private AnchorPane pane;
+
+    @FXML
+    ImageView ship;
+
+    @FXML
+    ImageView fish;
 
     @Override
     public void setAppController(Controller appController) {
         this.appController = appController;
     }
+
+    public void init() throws IOException {
+        Timeline tl = new Timeline();
+        tl = appController.setBackgroundAnimation(bg, 12, 0.2, pane);
+        tl.play();
+        pane.setOpacity(0.8);
+
+    };
 
     public void fishPressed(MouseEvent event) throws IOException{
         music();
@@ -43,24 +61,24 @@ public class selectSceneController implements SceneController {
 
     // Event handlers for mouse hover and exit (ship)
     public void shipEntered(MouseEvent event) {
-        System.out.println("Mouse entered the ship image.");
-        // Add hover effect logic here, e.g., changing opacity
+        //System.out.println("Mouse entered the ship image.");
+        ship.setOpacity(1);
     }
 
     public void shipExited(MouseEvent event) {
-        System.out.println("Mouse exited the ship image.");
-        // Add hover effect logic here, e.g., resetting opacity
+        //System.out.println("Mouse exited the ship image.");
+        ship.setOpacity(0.5);
     }
 
     // Event handlers for mouse hover and exit (FISH)
     public void fishEntered(MouseEvent event) {
-        System.out.println("Mouse entered the FISH image.");
-        // Add hover effect logic here, e.g., changing opacity
+        //System.out.println("Mouse entered the FISH image.");
+        fish.setOpacity(1);
     }
 
     public void fishExited(MouseEvent event) {
-        System.out.println("Mouse exited the FISH image.");
-        // Add hover effect logic here, e.g., resetting opacity
+        //System.out.println("Mouse exited the FISH image.");
+        fish.setOpacity(0.5);
     }
 
     MediaPlayer mediaPlayer;
